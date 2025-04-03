35cf2b19166457d8f46ebfdb91d58422b681e245
EarlAmematekpo16 <165102260+kwa0028@users.noreply.github.com>
2025-04-03 22:05:45 +0000

    changes int.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
+from auth import auth
+from views import views
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "h4?+Do^^XZVsxr^uT*g;6=8C*asvFj(/6Va;i=gZeI$Lvo!&fm;{f'^Zc:ZiuK|"
 
-    from .views import views
-    from .auth import auth
+    app.register_blueprint(views)
+    app.register_blueprint(auth)
 
-    app.register_blueprint(views, url_prefix ='/')
-    app.register_blueprint(auth, url_prefix ='/')
+    if __name__ == "__main__":
+        app.run(debug=True)
 
     return app
