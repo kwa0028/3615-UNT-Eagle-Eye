9a4f2e8a99b93b05c0f9445884ebe8430533e8de
EarlAmematekpo16 <165102260+kwa0028@users.noreply.github.com>
2025-04-03 22:53:34 +0000

    changed folder path

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from auth import auth
 from views import views
+import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder=os.path.abspath('.')) 
     app.config['SECRET_KEY'] = "h4?+Do^^XZVsxr^uT*g;6=8C*asvFj(/6Va;i=gZeI$Lvo!&fm;{f'^Zc:ZiuK|"
 
     app.register_blueprint(views)
     app.register_blueprint(auth)
 
-    if __name__ == "__main__":
-        app.run(debug=True)
-
     return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
--- backend/views.py
+++ backend/views.py
@@ -1,12 +1,13 @@
 from flask import Blueprint
 from flask import render_template
 
-views = Blueprint('auth', __name__)
+views = Blueprint('views', __name__)
+
 
 @views.route('/')
 def home():
     return render_template("index.html")
 
 @views.route('/about')
-def home():
+def about():
     return render_template("about.html")
